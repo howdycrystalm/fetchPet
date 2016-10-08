4cc01019b90b682bc98459bf24c46aa009455e61
howdycrystalm <howdycrystalm@gmail.com>
2016-10-08 16:03:37 -0600

    anchor working

--- js/controllers/petCtrl.js
+++ js/controllers/petCtrl.js
@@ -1,10 +1,15 @@
 angular.module('app')
-  .controller('petCtrl', function($scope, petService) {
+  .controller('petCtrl', function($scope, $location, $anchorScroll, petService) {
     
       petService.getRandom().then(function(response) {
         console.log(response)
           $scope.randomPets = response.data; 
   
       });
+      
+      $scope.scrollTo = function(scrollLocation){
+        $location.hash(scrollLocation);
+        $anchorScroll();
+      }  
     
   })
